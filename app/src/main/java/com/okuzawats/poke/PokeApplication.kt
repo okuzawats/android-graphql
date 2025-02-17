a40d43e117151db8da850580328e452485b8d8a6
okuzawats <okuzawats@gmail.com>
2025-02-18 06:25:18 +0900

    initialize logcat using startup

--- app/src/main/java/com/okuzawats/poke/PokeApplication.kt
+++ app/src/main/java/com/okuzawats/poke/PokeApplication.kt
@@ -1,17 +1,5 @@
 package com.okuzawats.poke
 
 import android.app.Application
-import logcat.AndroidLogcatLogger
-import logcat.LogPriority
 
-class PokeApplication : Application() {
-  override fun onCreate() {
-    super.onCreate()
-
-    // Log all priorities in debug builds, no-op in release builds.
-    AndroidLogcatLogger.installOnDebuggableApp(
-      application = this,
-      minPriority = LogPriority.VERBOSE,
-    )
-  }
-}
+class PokeApplication : Application()
