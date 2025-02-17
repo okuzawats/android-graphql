30c783f2cd3b311039532fcfb02f1815a84919ec
okuzawats <okuzawats@gmail.com>
2025-02-18 06:18:00 +0900

    add dependency on startup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
   implementation(libs.androidx.material3)
   implementation(libs.apollo.runtime)
   implementation(libs.logcat)
+  implementation(libs.startup)
   testImplementation(libs.junit)
   debugImplementation(libs.androidx.ui.tooling)
   debugImplementation(libs.androidx.ui.test.manifest)
