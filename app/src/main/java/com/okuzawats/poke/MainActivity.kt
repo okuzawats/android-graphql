51f4c5223fc032c75d287360eb6f51ac014eeaec
okuzawats <okuzawats@gmail.com>
2025-02-18 06:28:47 +0900

    reformat

--- app/src/main/java/com/okuzawats/poke/MainActivity.kt
+++ app/src/main/java/com/okuzawats/poke/MainActivity.kt
@@ -18,7 +18,9 @@ import kotlinx.coroutines.launch
 import logcat.logcat
 
 class MainActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
+  override fun onCreate(
+    savedInstanceState: Bundle?,
+  ) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContent {
@@ -48,7 +50,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(
+  name: String,
+  modifier: Modifier = Modifier,
+) {
   Text(
     text = "Hello $name!", modifier = modifier
   )
