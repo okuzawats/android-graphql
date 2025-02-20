2abd13539fddb03cdc8c86fbed6cbc943c320b30
okuzawats <okuzawats@gmail.com>
2025-02-21 05:19:55 +0900

    inject okhttpclient to apollo client

--- app/src/main/java/com/okuzawats/poke/apollo/ApolloClientFactory.kt
+++ app/src/main/java/com/okuzawats/poke/apollo/ApolloClientFactory.kt
@@ -1,6 +1,8 @@
 package com.okuzawats.poke.apollo
 
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.network.okHttpClient
+import okhttp3.OkHttpClient
 import javax.inject.Inject
 
 private const val END_POINT_URL = "https://beta.pokeapi.co/graphql/v1beta"
@@ -8,14 +10,16 @@ private const val END_POINT_URL = "https://beta.pokeapi.co/graphql/v1beta"
 /**
  * factory of [ApolloClient].
  */
-class ApolloClientFactory @Inject constructor() {
+class ApolloClientFactory @Inject constructor(
+  private val okHttpClient: OkHttpClient,
+) {
   /**
    * returns the instance of [ApolloClient].
    */
   fun create(): ApolloClient {
-    val apolloClient = ApolloClient.Builder()
+    return ApolloClient.Builder()
       .serverUrl(END_POINT_URL)
+      .okHttpClient(okHttpClient)
       .build()
-    return apolloClient
   }
 }
