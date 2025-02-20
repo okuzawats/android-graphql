e4da9c180b03a938bfde87fdd0341a306ec40180
okuzawats <okuzawats@gmail.com>
2025-02-21 05:05:40 +0900

    implement logging interceptor

---
+++ app/src/main/java/com/okuzawats/poke/okhttp/LoggingInterceptorFactory.kt
@@ -0,0 +1,21 @@
+package com.okuzawats.poke.okhttp
+
+import okhttp3.Interceptor
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Inject
+
+/**
+ * factory of [Interceptor] that logs request and response
+ */
+class LoggingInterceptorFactory @Inject constructor() {
+  /**
+   * returns the instance of [Interceptor]
+   */
+  fun create(): Interceptor {
+    val httpLoggingInterceptor = HttpLoggingInterceptor()
+    // log request and response header and body
+    httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+    return httpLoggingInterceptor
+  }
+}
