b4e7d3968bb52c35031254761413bbcac25966db
okuzawats <okuzawats@gmail.com>
2025-02-23 17:09:22 +0900

    initialize Activity with initializer

--- app/src/main/java/com/okuzawats/poke/PokeActivity.kt
+++ app/src/main/java/com/okuzawats/poke/PokeActivity.kt
@@ -2,34 +2,27 @@ package com.okuzawats.poke
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.okuzawats.poke.feature.version.VersionScreen
-import com.okuzawats.poke.ui.theme.PokeTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class PokeActivity : ComponentActivity() {
 
-  override fun onCreate(
-    savedInstanceState: Bundle?,
-  ) {
+  @Inject
+  lateinit var initializer: PokeActivityInitializer
+
+  override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    enableEdgeToEdge()
-    setContent {
-      PokeTheme {
-        Scaffold(
-          modifier = Modifier.fillMaxSize()
-        ) { innerPadding ->
-          VersionScreen(
-            modifier = Modifier.padding(innerPadding)
-          )
-        }
-      }
-    }
+    initializer.initialize(
+      activity = this,
+      content = { innerPadding ->
+        VersionScreen(
+          modifier = Modifier.padding(innerPadding)
+        )
+      },
+    )
   }
 }
