b7ba85ea560b38847b65579bfa3a063b884722f2
okuzawats <okuzawats@gmail.com>
2025-02-23 15:51:26 +0900

    create api module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
   alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.android.library) apply false
   alias(libs.plugins.apollo.plugin) apply false
   alias(libs.plugins.hilt.plugin) apply false
   alias(libs.plugins.kotlin.android) apply false
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Poke"
 include(":app")
+include(":api")
