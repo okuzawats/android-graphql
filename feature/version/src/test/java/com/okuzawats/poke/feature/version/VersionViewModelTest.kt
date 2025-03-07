01127bdfd909e6bafa4435f6e07238518394c7d8
okuzawats <okuzawats@gmail.com>
2025-03-08 04:58:56 +0900

    add test on failure

--- feature/version/src/test/java/com/okuzawats/poke/feature/version/VersionViewModelTest.kt
+++ feature/version/src/test/java/com/okuzawats/poke/feature/version/VersionViewModelTest.kt
@@ -1,6 +1,10 @@
 package com.okuzawats.poke.feature.version
 
+import com.okuzawats.poke.core.logger.Logger
+import io.mockk.coEvery
 import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 
@@ -9,18 +13,31 @@ class VersionViewModelTest {
   private lateinit var sut: VersionViewModel
 
   private lateinit var versionRepository: VersionRepository
+  private lateinit var logger: Logger
 
   @Before
   fun setup() {
     versionRepository = mockk()
+    logger = mockk(relaxUnitFun = true)
 
     sut = VersionViewModel(
       versionRepository = versionRepository,
-      logger = mockk(relaxUnitFun = true),
+      logger = logger,
     )
   }
 
   @Test
-  fun onEntered() {
+  fun onEntered() = runTest {
+    coEvery {
+      versionRepository.fetch()
+    } returns Result.failure(
+      Throwable("failure"),
+    )
+
+    sut.onEntered()
+
+    verify {
+      logger.v("failure")
+    }
   }
 }
