8f6b2af61c54b7c05ee73e873d3adf8a971d021a
okuzawats <okuzawats@gmail.com>
2025-03-08 06:26:38 +0900

    use also

--- app/src/main/java/com/okuzawats/poke/PokeActivity.kt
+++ app/src/main/java/com/okuzawats/poke/PokeActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -31,8 +30,9 @@ class PokeActivity : ComponentActivity() {
     enableEdgeToEdge()
     setContent {
       PokeTheme {
-        val navController: NavHostController = rememberNavController()
-        pokeNavigator.setupNavHostController(navController)
+        val navController = rememberNavController().also {
+          pokeNavigator.setupNavHostController(it)
+        }
 
         Scaffold(
           modifier = Modifier.fillMaxSize(),
